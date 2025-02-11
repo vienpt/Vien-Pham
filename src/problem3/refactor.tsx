877c1a685a4db1292d8a9c6670f77a2d1bf28e82
Vien Pham <viendev@outlook.com>
2025-02-11 11:55:06 +0700

    fix: update logic
    - separate function getPriority
    - updated interface
    - updated handle sorted and formatted

--- src/problem3/refactor.tsx
+++ src/problem3/refactor.tsx
@@ -1,79 +1,110 @@
+// Added import
+import { React, useMemo } from 'react'
+
 interface WalletBalance {
   currency: string
   amount: number
+  blockchain: string // Added missing blockchain property
 }
-interface FormattedWalletBalance {
-  currency: string
-  amount: number
+
+// Updated formatted by extends WalletBalance interface
+interface FormattedWalletBalance extends WalletBalance {
   formatted: string
 }
 
 interface Props extends BoxProps {}
+
+// Define enum blockchain
+enum BlockchainPriority {
+  Osmosis = 100,
+  Ethereum = 50,
+  Arbitrum = 30,
+  Zilliqa = 20,
+  Neo = 20,
+  Default = -99,
+}
+
+function getBlockChainPriority(blockchain: string): number {
+  const currentBlockChainPriority =
+    blockchain as keyof typeof BlockchainPriority
+
+  if (currentBlockChainPriority) {
+    return BlockchainPriority[currentBlockChainPriority]
+  }
+  return BlockchainPriority.Default
+}
+
 const WalletPage: React.FC<Props> = (props: Props) => {
   const { children, ...rest } = props
   const balances = useWalletBalances()
   const prices = usePrices()
 
-  const getPriority = (blockchain: any): number => {
-    switch (blockchain) {
-      case 'Osmosis':
-        return 100
-      case 'Ethereum':
-        return 50
-      case 'Arbitrum':
-        return 30
-      case 'Zilliqa':
-        return 20
-      case 'Neo':
-        return 20
-      default:
-        return -99
-    }
-  }
+  // Should be put to utils or composable hook relate to this page
+  // const getPriority = (blockchain: any): number => {
+  //   switch (blockchain) {
+  //     case 'Osmosis':
+  //       return 100
+  //     case 'Ethereum':
+  //       return 50
+  //     case 'Arbitrum':
+  //       return 30
+  //     case 'Zilliqa':
+  //       return 20
+  //     case 'Neo':
+  //       return 20
+  //     default:
+  //       return -99
+  //   }
+  // }
 
   const sortedBalances = useMemo(() => {
     return balances
       .filter((balance: WalletBalance) => {
-        const balancePriority = getPriority(balance.blockchain)
-        if (lhsPriority > -99) {
-          if (balance.amount <= 0) {
-            return true
-          }
+        const balancePriority = getBlockChainPriority(balance.blockchain)
+        // lhsPriority doesn't exists. Should be current balancePriority as assume
+        // updated condition with avoid nested conditional
+        if (
+          balancePriority > BlockchainPriority.Default &&
+          balance.amount <= 0
+        ) {
+          return true
         }
         return false
       })
       .sort((lhs: WalletBalance, rhs: WalletBalance) => {
-        const leftPriority = getPriority(lhs.blockchain)
-        const rightPriority = getPriority(rhs.blockchain)
-        if (leftPriority > rightPriority) {
-          return -1
-        } else if (rightPriority > leftPriority) {
-          return 1
-        }
-      })
-  }, [balances, prices])
+        const leftPriority = getBlockChainPriority(lhs.blockchain)
+        const rightPriority = getBlockChainPriority(rhs.blockchain)
+        // Nothing special sort here. Just make simply
+        return rightPriority - leftPriority
+        // if (leftPriority > rightPriority) {
+        //   return -1
+        // } else if (rightPriority > leftPriority) {
+        //   return 1
+        // }
+      }) as WalletBalance[] // added cast type here for using in other function
+  }, [balances]) // removed redundant prices dependencies
 
+  // could put formattedBalances into useWalletBalances hook
   const formattedBalances = sortedBalances.map((balance: WalletBalance) => {
     return {
       ...balance,
       formatted: balance.amount.toFixed(),
-    }
+    } as FormattedWalletBalance
   })
 
-  const rows = sortedBalances.map(
-    (balance: FormattedWalletBalance, index: number) => {
-      const usdValue = prices[balance.currency] * balance.amount
-      return (
-        <WalletRow
-          className={classes.row}
-          key={index}
-          amount={balance.amount}
-          usdValue={usdValue}
-          formattedAmount={balance.formatted}
-        />
-      )
-    }
-  )
+  // It's seems function are using formattedBalances
+  const rows = formattedBalances.map((balance, index: number) => {
+    const usdValue = prices[balance.currency] * balance.amount
+    return (
+      <WalletRow
+        className={classes.row}
+        key={`${balance.currency}-${index}`} // determine key instead just index
+        amount={balance.amount}
+        usdValue={usdValue}
+        formattedAmount={balance.formatted}
+      />
+    )
+  })
 
   return <div {...rest}>{rows}</div>
 }
